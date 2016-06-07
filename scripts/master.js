861428990ea7f2003612ba2a660a1c55eb97dc05
Yvonne Hayes <queyvonne@gmail.com>
2016-06-07 11:14:00 -0700

    started overhauling master.js away from superagent to ajax calls

--- scripts/master.js
+++ scripts/master.js
@@ -3,6 +3,35 @@ $(function () {
 
   $('form').on('submit', function(e){
     e.preventDefault();
+
+    $.ajax({
+      url: e.target.action,
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        username: e.target.elements.username.value,
+        password: e.target.elements.password.value
+      }
+    }).done(function(data) {
+      if (!err && res.body && res.body.status == 'success' && res.body.result) {
+        localStorage.token = res.body.result;
+        $('.nav-tabs').before('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><div class="form-group"><label>Your Token</label><input class="form-control" type="text" value="'+res.body.result+'"></div></div>');
+      } else {
+        var error = err;
+
+        console.log('username: ' + e.target.elements.username.value);
+        if (res.body.result.message !== undefined) {
+          error = res.body.result.message;
+        }else if(res.body.result){
+          error = res.body.result;
+        }
+
+        $('.nav-tabs').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+error+'</div>');
+      }
+    })
+
     request
       .post(e.target.action)
       .set('Content-Type', 'application/json')
