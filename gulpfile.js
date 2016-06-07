aa8fb2c1fd1fed126cc0a62acd0e3cc41781fb37
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-06 22:48:23 -0700

    Created clean, concat, uglify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
 const stylus = require('gulp-stylus');
+const uglify = require('gulp-uglify');
+
+gulp.task('run-clean', () => {
+  return gulp.src('./public')
+    .pipe(clean());
+});
+
+gulp.task('run-concat-uglify', () => {
+  return gulp.src(['./scripts/master.js'])
+    .pipe(concat('master.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/scripts'));
+});
 
 gulp.task('run-lint', () => {
   return gulp.src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
-
-gulp.task('run-test', () => {
-  return gulp.src('./test/**/*.js', {read: false})
-    .pipe(mocha());
 });
 
 gulp.task('run-nodemon', () => {
@@ -25,12 +35,23 @@ gulp.task('run-nodemon', () => {
 
 gulp.task('run-stylus', () => {
   return gulp.src('./styles/master.styl')
-    .pipe(stylus({
-      compress: true
-    }))
+    .pipe(stylus({compress: true}))
     .pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task('default', ['run-stylus']);
-gulp.task('dev', ['run-stylus', 'run-nodemon']);
-gulp.task('test', ['run-lint', 'run-test']);
+gulp.task('run-test', () => {
+  return gulp.src('./test/**/*.js', {read: false})
+    .pipe(mocha());
+});
+
+gulp.task('build', ['run-clean'], () => {
+  return gulp.start(['run-stylus', 'run-concat-uglify']);
+});
+
+gulp.task('dev', ['build'], () => {
+  return gulp.start(['run-nodemon']);
+});
+
+gulp.task('test', ['build'], () => {
+  return gulp.start(['run-lint', 'run-test']);
+});
