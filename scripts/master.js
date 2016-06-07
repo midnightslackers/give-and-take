d4e189d8dc25c8d27568591ca1b03eec66a38592
Yvonne Hayes <queyvonne@gmail.com>
2016-06-07 11:43:40 -0700

    sign up now actually works. Hurrah

--- scripts/master.js
+++ scripts/master.js
@@ -1,60 +1,59 @@
 $(function () {
-  var request = superagent;
+  // var request = superagent;
 
   $('form').on('submit', function(e){
     e.preventDefault();
 
     $.ajax({
       url: e.target.action,
-      type: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      data: {
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify({
         username: e.target.elements.username.value,
         password: e.target.elements.password.value
-      }
-    }).done(function(data) {
-      if (!err && res.body && res.body.status == 'success' && res.body.result) {
-        localStorage.token = res.body.result;
-        $('.nav-tabs').before('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><div class="form-group"><label>Your Token</label><input class="form-control" type="text" value="'+res.body.result+'"></div></div>');
+      }),
+      type: 'POST'
+    }
+    ).done(function(data) {
+      if (data.status == 'success' && data.result) {
+        localStorage.token = data.result;
+        $('.nav-tabs').before('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><div class="form-group"><label>Your Token</label><input class="form-control" type="text" value="'+data.result+'"></div></div>');
       } else {
         var error = err;
 
-        console.log('username: ' + e.target.elements.username.value);
-        if (res.body.result.message !== undefined) {
-          error = res.body.result.message;
-        }else if(res.body.result){
-          error = res.body.result;
+        if (data.result.message !== undefined) {
+          error = data.result.message;
+        }else if(data.result){
+          error = data.result;
         }
 
         $('.nav-tabs').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+error+'</div>');
       }
-    })
+    });
 
-    request
-      .post(e.target.action)
-      .set('Content-Type', 'application/json')
-      .send( {
-        username: e.target.elements.username.value,
-        password: e.target.elements.password.value
-      })
-      .end(function(err, res) {
-        if (!err && res.body && res.body.status == 'success' && res.body.result) {
-          localStorage.token = res.body.result;
-          $('.nav-tabs').before('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><div class="form-group"><label>Your Token</label><input class="form-control" type="text" value="'+res.body.result+'"></div></div>');
-        } else {
-          var error = err;
-
-          console.log('username: ' + e.target.elements.username.value);
-          if (res.body.result.message !== undefined) {
-            error = res.body.result.message;
-          }else if(res.body.result){
-            error = res.body.result;
-          }
-
-          $('.nav-tabs').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+error+'</div>');
-        }
-      });
+//     request
+//       .post(e.target.action)
+//       .set('Content-Type', 'application/json')
+//       .send( {
+//         username: e.target.elements.username.value,
+//         password: e.target.elements.password.value
+//       })
+//       .end(function(err, res) {
+//         if (!err && res.body && res.body.status == 'success' && res.body.result) {
+//           localStorage.token = res.body.result;
+//           $('.nav-tabs').before('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><div class="form-group"><label>Your Token</label><input class="form-control" type="text" value="'+res.body.result+'"></div></div>');
+//         } else {
+//           var error = err;
+//
+//           console.log('username: ' + e.target.elements.username.value);
+//           if (res.body.result.message !== undefined) {
+//             error = res.body.result.message;
+//           }else if(res.body.result){
+//             error = res.body.result;
+//           }
+//
+//           $('.nav-tabs').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+error+'</div>');
+//         }
+      // });
   });
 });
