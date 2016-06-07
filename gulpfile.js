f9495a050b3b7a73b5b906bad4ec4e3dd58a87c9
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-06 21:23:19 -0700

    Created stylus task

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,35 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
+const stylus = require('gulp-stylus');
 
 gulp.task('run-lint', () => {
-  gulp.src(['**/*.js', '!node_modules/**'])
+  return gulp.src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task('run-test', () => {
-  gulp.src('./test/**/*.js', {read: false})
+  return gulp.src('./test/**/*.js', {read: false})
     .pipe(mocha());
 });
 
 gulp.task('run-nodemon', () => {
-  nodemon({
+  return nodemon({
     script: 'index.js',
     ext: 'js'
   });
 });
 
-gulp.task('dev', ['run-nodemon']);
+gulp.task('run-stylus', () => {
+  return gulp.src('./styles/master.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('default', ['run-stylus']);
+gulp.task('dev', ['run-stylus', 'run-nodemon']);
 gulp.task('test', ['run-lint', 'run-test']);
